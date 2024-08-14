4235ec06e58d703a1e8cfe83909aad8d6d5e033c
red2fred2 <red2fred2base@live.com>
2024-08-14 02:34:34 -0500

    Add single task get call to API This commit also changes the user tasks call to /userTasks/:userID to be more consistent.

--- backend/taskapi/index.js
+++ backend/taskapi/index.js
@@ -5,6 +5,13 @@ import Task from './model/Task.js';
 import User from './model/User.js';
 
 await mongoose.connect('mongodb://127.0.0.1/todo');
+//66bc462fe7c703c393ce9c88
+
+// await Task.create({name: "task", description: "This is a thing that I need to do at some point", dueDate: Date.now()});
+
+await Task.deleteMany({name: "task"});
+
+// mongoose.disconnect();
 
 const port = 3050;
 const taskapi = express();
@@ -14,18 +21,35 @@ taskapi.listen(port, () => {
 });
 
 // Get all tasks from a single user
-taskapi.get('/:userID/tasks', async (req, res) => {
+taskapi.get('/userTasks/:userID', async (req, res) => {
 	let userID = req.params.userID;
 
 	// Handle the user not existing
-	let existingDocuments = await User.exists({_id: userID}).exec();
-	if(existingDocuments === null) {
+	let exists = await User.findById(userID).exec();
+	if(exists === null) {
 		return res.status(404)
 				  .json({error: "User does not exist"});
 	}
 
 	// Otherwise, return the tasks
-	let result = await Task.find({owner: userID}).exec();
+	let result = await Task.find({name: "task"}).exec();
+
+	res.json(result);
+});
+
+// Get a single task
+taskapi.get('/task/:taskID', async (req, res) => {
+	let taskID = req.params.taskID;
+
+	// Handle the task not existing
+	let exists = await Task.findById(taskID).exec();
+	if(exists === null) {
+		return res.status(404)
+				  .json({error: "Task does not exist"});
+	}
+
+	// Otherwise, return the tasks
+	let result = await Task.findById(taskID).exec();
 
 	res.json(result);
 });
